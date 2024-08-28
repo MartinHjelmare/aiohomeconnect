95781265581a8231b39c76ba9c58f7df606702d5
Martin Hjelmare <marhje52@gmail.com>
2024-08-28 10:46:33 +0200

    feat: add base client (#2)
    * chore: add httpx
    
    * feat: add abstract auth
    
    * test: test abstract auth
    
    * feat: add cli to authorize and get token
    
    * feat: add crude client without model
    
    * chore: add fastapi for cli
    
    * chore: add docstring
    
    * chore: add uvicorn for cli
    
    * test: fix test

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+"""Provide common fixtures for tests."""
+from collections.abc import AsyncGenerator
+
+from httpx import AsyncClient
+import pytest
+
+
+@pytest.fixture
+async def httpx_client() -> AsyncGenerator[AsyncClient, None]:
+    """Return an authenticated HTTP client."""
+    async with AsyncClient() as client:
+        yield client
---
+++ tests/test_client.py
@@ -0,0 +1,35 @@
+"""Test the client."""
+from httpx import AsyncClient
+from pytest_httpx import HTTPXMock
+
+from aiohomeconnect.client import AbstractAuth
+
+TEST_ACCESS_TOKEN = "1234"
+
+
+class AuthClient(AbstractAuth):
+    """Use the abstract auth."""
+
+    async def async_get_access_token(self) -> str:
+        """Return a valid access token."""
+        return TEST_ACCESS_TOKEN
+
+
+async def test_abstract_auth(httpx_client: AsyncClient, httpx_mock: HTTPXMock) -> None:
+    """Test the abstract auth."""
+    url_query = "key1=value1&key2=value2"
+    httpx_mock.add_response(
+        url=f"https://example.com/test?{url_query}",
+        json={"test": "test_result"},
+    )
+
+    params = {"key1": "value1", "key2": "value2"}
+    client = AuthClient(httpx_client, "https://example.com")
+    response = await client.request("GET", "/test", params=params)
+
+    assert response.status_code == 200
+    assert response.json() == {"test": "test_result"}
+    request = httpx_mock.get_request()
+    assert request
+    assert request.headers["authorization"] == f"Bearer {TEST_ACCESS_TOKEN}"
+    assert request.url.query.decode(encoding="utf-8") == "key1=value1&key2=value2"
