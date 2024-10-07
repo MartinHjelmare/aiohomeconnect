6fdbaa8b219137c8ed952df89c5c88033d053b8e
Martin Hjelmare <marhje52@gmail.com>
2024-10-07 18:16:31 +0200

    chore: clean up dummy modules (#13)

--- src/aiohomeconnect/main.py
+++
@@ -1,6 +0,0 @@
-"""Dummy module for testing."""
-
-
-def add(n1: int, n2: int) -> int:
-    """Add the arguments."""
-    return n1 + n2
--- tests/test_main.py
+++
@@ -1,8 +0,0 @@
-"""Test the main module."""
-
-from aiohomeconnect.main import add
-
-
-def test_add() -> None:
-    """Adding two number works as expected."""
-    assert add(1, 1) == 2
