e499d149099c51c9c3d4db5db8bbe7a23fabddff
J. Diego Rodríguez Royo <jdrr1998@hotmail.com>
2026-06-09 19:31:01 +0200

    feat: mapped keys

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,7 @@
 """Tests for the model module."""
 
+import pytest
+
 from aiohomeconnect.model import EventKey, OptionKey, ProgramKey, SettingKey, StatusKey
 
 
@@ -43,3 +45,37 @@ async def test_options_settings_status_references_at_events() -> None:
             assert event_key in StatusKey.__members__.values(), (
                 f"EventKey.{event_key.name} not in StatusKey enumeration"
             )
+
+
+@pytest.mark.parametrize(
+    ("str_key", "expected_enum_key"),
+    [
+        (
+            "LaundryCare.WasherDryer.Program.Cotton.Cotton.Cotton",
+            ProgramKey.LAUNDRY_CARE_WASHER_DRYER_COTTON,
+        ),
+        (
+            "LaundryCare.WasherDryer.Program.DelicatesSilk.DelicatesSilk.DelicatesSilk",
+            ProgramKey.LAUNDRY_CARE_WASHER_DRYER_DELICATES_SILK,
+        ),
+        (
+            "LaundryCare.WasherDryer.Program.DrumCleanDry.DrumCare.DrumCare",
+            ProgramKey.LAUNDRY_CARE_WASHER_DRYER_DRUM_CLEAN_AND_DRY_DRUM_CARE,
+        ),
+        (
+            "LaundryCare.WasherDryer.Program.Rinse.Rinse.Rinse",
+            ProgramKey.LAUNDRY_CARE_WASHER_DRYER_RINSE,
+        ),
+        (
+            "LaundryCare.WasherDryer.Program.Sensitive.Sensitive.Sensitive",
+            ProgramKey.LAUNDRY_CARE_WASHER_DRYER_SENSITIVE,
+        ),
+        (
+            "LaundryCare.WasherDryer.Program.Wool.Wool.Wool",
+            ProgramKey.LAUNDRY_CARE_WASHER_DRYER_WOOL,
+        ),
+    ],
+)
+async def test_mapped_program_keys(str_key: str, expected_enum_key: ProgramKey) -> None:
+    """Test that the string keys are correctly mapped to the enum keys."""
+    assert ProgramKey(str_key) is expected_enum_key
